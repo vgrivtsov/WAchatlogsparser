622e6ff283704315ed475ac54c2bf7315ad13f49
vgrivtsov <vgrivtsov@gmail.com>
2017-03-20 20:19:46 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ from werkzeug import secure_filename
 from werkzeug.contrib.fixers import ProxyFix
 from wa_main import main as file_parsing
 
-
 app = Flask(__name__)    
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['ALLOWED_EXTENSIONS'] = set(['txt'])  
@@ -26,16 +25,13 @@ def index_template():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    a = timeit.default_timer() # test time job of parsing script
     file = request.files['file']
 
     if file and allowed_file(file.filename):
-
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
-        data_dict = file_parsing(filename)
-        
-        print("TIME OF PARSING FUNCTION: "+str(timeit.default_timer()-a))
+        data_dict = file_parsing(filename)        
+
         return render_template('stats.html', data_dict=data_dict[0],\
                file_status="File %s processed success" % filename,\
                users_data=data_dict[1])
@@ -43,10 +39,7 @@ def upload():
     else:
         filename=file.filename
         return render_template('layout.html', file_status="File %s not TXT!" % filename)
-
-#app.wsgi_app = ProxyFix(app.wsgi_app)
-   
+  
 if __name__ == '__main__':
-
     app.run(host="0.0.0.0", debug=True, port=80)
-    #sys.exit(main())
+
